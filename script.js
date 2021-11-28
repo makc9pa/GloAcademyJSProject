91a1a140c9055c2c2f2aeb87992f6c133b0ca6b0
makc9pa <2max2@mail.ru>
2021-11-28 23:01:20 +0300

    feat(lesson02): создание новых переменных, вывод в консоль

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Корпоративный портал в дополненную реальность';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 969;
+let rollback = 14;
+let fullPrice = 10005005;
+let adaptive = true;
 
-alert("Some alert");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("Some log text");
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`);
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log(Math.trunc(fullPrice * (rollback/100))); 
