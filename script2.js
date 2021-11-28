627c11ad0fadbaeab926b2b90322bd90df83e6d2
makc9pa <2max2@mail.ru>
2021-11-28 23:58:52 +0300

    feat(lesson02_hard): преобразовал число в массив цифр, перемножил их, возвел в степень результат перемножения и вывел первые две цифры в консоль

--- script2.js
+++ script2.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+let num = 266219;
+
+// Преобразует число в строку, принимает каждый символ этой строки, 
+// преобразует его в число и помещает в новый массив. 
+// Этот новый массив чисел будет возвращен.
+let arrayOfDigits = Array.from(String(num), Number);
+
+let multiplication = arrayOfDigits.reduce((acc, rec) => acc * rec);
+
+let exponentiation = multiplication ** 3;
+
+console.log(exponentiation.toString().substring(0, 2));
