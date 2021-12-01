2fd5175e571537e8f791b6829c8afe403f068271
makc9pa <2max2@mail.ru>
2021-12-01 19:38:17 +0300

    fix(lesson03): удалил лишние объявления переменных, сменил тип переменных let на const

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 'use strict';
 
 const ROLLBACK = 1000;
-let fullPrice = 0;
-let servicePercentPrice = 0;
 
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?', '100');
-let adaptive = !!prompt('Нужен ли адаптив на сайте?', true);
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?', '100');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?', '100');
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '100');
+const adaptive = !!prompt('Нужен ли адаптив на сайте?', true);
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?', '100');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?', '100');
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 switch (true) {
     case fullPrice >= 30000:
@@ -31,6 +29,6 @@ switch (true) {
         console.log('Что то пошло не так');
 }
 
-servicePercentPrice = Math.ceil(fullPrice - ROLLBACK);
+const servicePercentPrice = Math.ceil(fullPrice - ROLLBACK);
 
 console.log(servicePercentPrice);
