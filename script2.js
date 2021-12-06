888f1bef96d51b1340e5f498ab8e3ebdf1b2e2ea
makc9pa <2max2@mail.ru>
2021-12-06 13:44:46 +0300

    feat(lesson02_hard): исправил тип переменных на const

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
-let num = 266219;
+const num = 266219;
 
 // Преобразует число в строку, принимает каждый символ этой строки, 
 // преобразует его в число и помещает в новый массив. 
 // Этот новый массив чисел будет возвращен.
-let arrayOfDigits = Array.from(String(num), Number);
+const arrayOfDigits = Array.from(String(num), Number);
 
-let multiplication = arrayOfDigits.reduce((acc, rec) => acc * rec);
+const multiplication = arrayOfDigits.reduce((acc, rec) => acc * rec);
 
-let exponentiation = multiplication ** 3;
+const exponentiation = multiplication ** 3;
 
 console.log(exponentiation.toString().substring(0, 2));
