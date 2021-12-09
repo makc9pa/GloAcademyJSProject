5c91fca1c31bb41e51a22e19bd1b33a85bb14d30
makc9pa <2max2@mail.ru>
2021-12-09 22:23:17 +0300

    feat(lesson06_hard): доработал логику  игры 'отгадай загаданное случайное число от 1 до 100'

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
 'use strict';
 
-let secretNumber = Math.round(Math.random() * 100)
+let secretNumber = Math.round(Math.random() * 10)
+let num
+let count = 10
+let confirmStatus
 
 const isIntegerNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num) && (num % 1 === 0)
 }
 
 function inputNumber () {
-    let num = prompt('Введи число')
+
+    if (count === 0) {
+        confirmStatus = confirm('Попытки закончились, хотите сыграть еще?')
+    }
+
+    if (confirmStatus) {
+        count = 10
+        secretNumber = Math.round(Math.random() * 100)
+        inputNumber()
+    }
+
+    if (!confirmStatus) {
+        alert('Игра окончена')
+    }
+
+    num = prompt('Введи число')
+
     if (num === null) {
         alert('Игра окончена')
-    } else {
-        num = num.trim()
-        !isIntegerNumber(num) ? inputNumber() : num = +num
-        if (num > secretNumber) {
-            alert('Загаданное число меньше')
-            inputNumber()
-        } else if (num < secretNumber) {
-            alert('Загаданное число больше')
-            inputNumber() 
-        } else {
-            alert('Поздравляю, Вы угадали!!!')
-            console.log(num)
-        }
+    } 
+
+    num = num.trim()
+    !isIntegerNumber(num) ? inputNumber() : num = +num
+
+    if (num > secretNumber) {
+        count--
+        alert(`Загаданное число меньше, осталось попыток: ${count}`)
+        inputNumber()
+    } 
+
+    if (num < secretNumber) {
+        count--
+        alert(`Загаданное число больше, осталось попыток: ${count}`)
+        inputNumber() 
+    } 
+
+    if (num === secretNumber) {
+        confirmStatus = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
+        console.log(secretNumber)
     }
+
+    if (confirmStatus) {
+        count = 10
+        secretNumber = Math.round(Math.random() * 100)
+        inputNumber()
+    }
+
+    if (!confirmStatus) {
+        alert('Игра окончена')
+    }
+
 }
 
 alert('Угадай загаданное число от 1 до 100')
 inputNumber()
-
-console.log(secretNumber)
