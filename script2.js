b0fd5c2a6e5513a0fb53099679cd4b3d415ff02a
makc9pa <2max2@mail.ru>
2021-12-15 19:36:07 +0300

    refactor(lesson07_hard): еще один вариант вывода дней недели

--- script2.js
+++ script2.js
@@ -1,14 +1,24 @@
-const arr_week = ['воскресение', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота']
-const now = new Date()
+'use strict';
 
-for (let i = 0; i < arr_week.length; i++) {
-    if (i === now.getDay() && (i === 0 || i === 6)) {
-        document.body.insertAdjacentHTML('beforeend', '<b>' + '<i>'+ arr_week[i] + '</i>' + '</b>' + '<br>');
-    } else if (i === now.getDay()) {
-        document.body.insertAdjacentHTML('beforeend', '<b>' + arr_week[i] + '</b>' + '<br>');
-    } else if (i >= 1 && i <= 5) {
-        document.body.insertAdjacentHTML('beforeend', arr_week[i] + '<br>');
-    } else {
-        document.body.insertAdjacentHTML('beforeend', '<i>' + arr_week[i] + '</i>' + '<br>');
-    }
+const 
+daysOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+today = new Date()
+
+// for (let i = 0; i < daysOfWeek; i++) {
+//     if (i === now.getDay() && (i === 0 || i === 6)) {
+//         document.body.insertAdjacentHTML('beforeend', '<b>' + '<i>'+ arr_week[i] + '</i>' + '</b>' + '<br>');
+//     } else if (i === now.getDay()) {
+//         document.body.insertAdjacentHTML('beforeend', '<b>' + arr_week[i] + '</b>' + '<br>');
+//     } else if (i >= 1 && i <= 5) {
+//         document.body.insertAdjacentHTML('beforeend', arr_week[i] + '<br>');
+//     } else {
+//         document.body.insertAdjacentHTML('beforeend', '<i>' + arr_week[i] + '</i>' + '<br>');
+//     }
+// }
+
+for (let i = 0; i < daysOfWeek.length; i++) {
+let day = i + 1 > 5 ? daysOfWeek[i].italics() : daysOfWeek[i];
+
+document.body.appendChild(document.createElement('p')).innerHTML =
+`${i + 1 !== today.getDay() ? day : day.bold()}`;
 }
